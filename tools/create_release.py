c4fade4760cb7af4c43730ca0802ff2670d06a45
Ray Yang <ruiguo@gmail.com>
2018-07-07 17:56:35 -0700

    Create new version
    Prompt users if they want to publish.

--- tools/create_release.py
+++ tools/create_release.py
@@ -17,19 +17,33 @@ def get_latest_tag():
     return latest_tag
 
 
-def calculate_new_version():
-    latest_tag = get_latest_tag()
-    pass
+def calculate_new_version(latest_tag):
+    major, minor, patch = latest_tag.split('.')
+    new_minor = int(minor) + 1
+    new_version = "{}.{}.0".format(major, str(new_minor))
+    print("New version : {}".format(new_version))
+    return new_version
 
 
 def publish_new_version(new_version):
-    pass
+    print("publishing new version: " + new_version)
+
+
+def should_publish():
+    answer = input("Publish the new version? (y/N)")
+    if answer == 'y':
+        return True
+    return False
 
 
 def create_release():
     update_tags()
-    new_version = calculate_new_version()
-    publish_new_version(new_version)
+    latest_tag = get_latest_tag()
+    new_version = calculate_new_version(latest_tag)
+    if should_publish():
+        publish_new_version(new_version)
+    else:
+        print("Abort.")
 
 
 if __name__ == "__main__":
